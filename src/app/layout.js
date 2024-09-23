a45f20bbe4dd4f660f9b48dacbf51100255109d2
Ali <126307892+alicode43@users.noreply.github.com>
2024-09-23 19:31:19 +0530

    add fav icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,11 +6,15 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: "Rustam's Porfolio",
   description:  "Rustam's Porfolio",
+
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+        <head>
+        <link rel="icon" href="/images/hero-image.png" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
