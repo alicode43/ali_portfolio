d592c84033315e4e0f5d603eb770c3c495cbcb9b
Ali <126307892+alicode43@users.noreply.github.com>
2024-09-23 18:21:21 +0530

    first commit

---
+++ src/app/components/AboutSection.tsx
@@ -0,0 +1,104 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>MongoDB</li>
+        <li>React Js</li>
+        <li>Next Js</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <p>Batchelor of Engineering From</p>
+        <b>VKMV Engineering College Salem</b>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>MongoDB Node.js Developer Path</li>
+        <li>Postman API Fundamentals Student Expert</li>
+        <li>The Complete 2024 Web Development Bootcamp</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <img src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-sm">
+            I am a passionate Full Stack Developer specializing in the <b> MERN
+            stack </b> . Currently a final-year
+            student, I have honed my skills through a rewarding 6-month onsite
+            internship, where I collaborated with a dynamic team on the Dotzza
+            project. 
+            <br /> <br /> 
+            With over <b>350</b> problems solved on LeetCode, I’m dedicated to
+            mastering algorithms and enhancing my coding proficiency. My efforts
+            were recognized with the <b>Best Engineer Award</b>, reflecting my
+            commitment to excellence. I have successfully completed multiple
+            full stack projects, utilizing technologies like Next.js and MongoDB.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 flex justify-between">
+        <span>Ali </span>
+        <span> </span>
+        <pre className="text-slate-600"> All rights reserved.</pre>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
